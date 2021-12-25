a1876c0c905334f882e165d4533e7884f14bfa80
Jawahar1197 <96660142+Jawahar1197@users.noreply.github.com>
2021-12-25 08:35:21 -0800

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Factorial {
+public static void main(String[] args) {
+	int i, fact = 1;
+	int j = 5;
+	for(i=1;i<=j;i++) {
+		fact = fact*i;
+		System.out.println("5*4*3*2*1 =" +fact);
+	}	
+}
+	
+
+}
